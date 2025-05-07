23dd39d0eda82891d657b7b147ade17fdb3fe014
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-07 22:12:56 +0530

    update

--- my-app/components/FormGeneratoer.tsx
+++ my-app/components/FormGeneratoer.tsx
@@ -65,6 +65,53 @@ const FormGenerator: React.FC<FormGeneratorProps> = ({
           {...register(name)}
         />
       )}
+
+      {fieldType === "currency" && (
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-sm text-gray-500 dark:text-gray-400">
+            $
+          </span>
+          <Input
+            id={name}
+            type="text"
+            className="!h-12 shadow-none placeholder:!text-muted-foreground pl-9"
+            disabled={disabled}
+            placeholder={placeholder || label}
+            defaultValue={defaultValue}
+            {...(register(name),
+            {
+              onChange: (e) => {
+                const rawValue = e.target.value
+                  ? e.target.value.replace(/\D/g, "")
+                  : "";
+                const formattedValue = rawValue
+                  ? new Intl.NumberFormat().format(Number(rawValue))
+                  : "";
+                e.target.value = formattedValue;
+              },
+              setValueAs: (value) => {
+                if (typeof value === "string" || typeof value === "number") {
+                  const cleanedValue = String(value).replace(/\D/g, "");
+                  return cleanedValue ? Number(cleanedValue) : null;
+                }
+                return null;
+              },
+            })}
+          />
+        </div>
+      )}
+
+      {fieldType === "phone" && (
+        <Input
+          id={name}
+          type="number"
+          className="!h-12 shadow-none placeholder:!text-muted-foreground"
+          disabled={disabled}
+          placeholder={placeholder || label}
+          defaultValue={defaultValue}
+          {...register(name)}
+        />
+      )}
     </div>
   );
 };
